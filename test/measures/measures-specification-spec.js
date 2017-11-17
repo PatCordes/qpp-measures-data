583c5d8e8c60c9c29a039f6b9cde3bcff0f9c331
Biju Joseph <biju.joseph@semanticbits.com>
2017-11-17 00:06:26 -0500

    setting timeouts to max 2 minutes

--- test/measures/measures-specification-spec.js
+++ test/measures/measures-specification-spec.js
@@ -8,7 +8,7 @@ const measuresData = require('../../index.js');
 function checkUrl(s) {
   return rp({method: 'HEAD', uri: s.url})
     .then(body => {
-      let valid = /4\d\d/.test(body.statusCode) === false;
+      const valid = /4\d\d/.test(body.statusCode) === false;
       return ({
         measureId: s.measureId,
         submissionMethod: s.url,
@@ -20,21 +20,22 @@ function checkUrl(s) {
 
 describe('measures specification', function() {
   it('should have valid specification links', function(done) {
-    let specs = [];
-    let measures = measuresData.getMeasuresData();
+    this.timeout(120000); // 2 minutes timeout.
+    const specs = [];
+    const measures = measuresData.getMeasuresData();
     measures
       .map(m => ({measureId: m.measureId, measureSpecification: m.measureSpecification}))
       .filter(s => !!s.measureSpecification)
       .forEach(s => {
         Object.keys(s.measureSpecification).forEach(key => {
-          let url = s.measureSpecification[key];
+          const url = s.measureSpecification[key];
           specs.push({measureId: s.measureId, url: url});
         });
       });
 
     Promise.map(specs, s => checkUrl(s))
       .then(results => {
-        let failuers = results.filter(r => !r.success);
+        const failuers = results.filter(r => !r.success);
         if (failuers.length > 0) {
           console.log(failuers);
         }
