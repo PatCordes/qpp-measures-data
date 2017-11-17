d788b07c15eb930f84ad6bc70bbd01f46c5f9477
Biju Joseph <biju.joseph@semanticbits.com>
2017-11-16 23:28:01 -0500

    addressing review comments

--- test/measures/measures-specification-spec.js
+++ test/measures/measures-specification-spec.js
@@ -1,35 +1,47 @@
 const chai = require('chai');
 const assert = chai.assert;
-var request = require('sync-request');
+const rp = require('request-promise');
+const Promise = require('bluebird');
 
-var measuresData = require('../../index.js');
+const measuresData = require('../../index.js');
+
+function checkUrl(s) {
+  return rp({method: 'HEAD', uri: s.url})
+    .then(function (body) {
+        let valid = /4\d\d/.test(body.statusCode) === false;
+        return ({measureId: s.measureId,
+          submissionMethod: s.url,
+          success: valid,
+          httpStatus: body.statusCode});
+    });
+};
 
 describe('measures specification', function() {
+
   it('should have valid specification links', function(done) {
     this.timeout(300000); // 5 minutes timeout.
-
-    var measures = measuresData.getMeasuresData();
-    var specifications = measures
+    let specs = [];
+    let measures = measuresData.getMeasuresData();
+    measures
       .map(m => ({measureId: m.measureId, measureSpecification: m.measureSpecification}))
-      .filter(s => !!s.measureSpecification);
-
-    var statuses = [];
-    specifications.forEach(s => {
-      Object.keys(s.measureSpecification).forEach(key => {
-        var url = s.measureSpecification[key];
-        console.log('Checking: ', url);
-        var body = request('HEAD', url);
-        var valid = /4\d\d/.test(body.statusCode) === false;
-        if (!valid) {
-          statuses.push({measureId: s.measureId, submissionMethod: key, httpStatus: body.statusCode});
-        }
-      });
+      .filter(s => !!s.measureSpecification)
+      .forEach(s => {
+        Object.keys(s.measureSpecification).forEach(key => {
+          let url = s.measureSpecification[key];
+          specs.push({measureId: s.measureId, url: url});
+        });
     });
 
-    if (statuses.length > 0) {
-      console.table(statuses);
-    }
-    assert.equal(0, statuses.length, 'One or more measure specifications link is invalid');
-    done();
+    Promise.map(specs, s => checkUrl(s))
+    .then(results => {
+      let failuers = results.filter(r => !r.success);
+      if (failuers.length > 0) {
+        console.log(failuers);
+      }
+      assert.equal(0, failuers.length, 'One or more measure specifications link is invalid');
+      done();
+    });
+
+
   });
 });
